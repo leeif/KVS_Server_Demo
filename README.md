f5cc281c32d4c61e6fc0dd94e11434a8b3438334
Yifan Li <yifan.li@G-PC-00433854.intra.gree-office.net>
2018-05-30 18:44:02 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+KVS_Server_Demo project was created with [fido](https://github.com/evilsocket/fido).
---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <iostream>
+using namespace std;
+
+
+class TCPServer {
+private:
+    /* data */
+    void InitServer() {
+
+    }
+
+public:
+    TCPServer(/* args */) {
+        InitServer();
+    }
+    ~TCPServer();
+};
+
+TCPServer::TCPServer(/* args */) {
+
+}
+
+TCPServer::~TCPServer() {
+
+}
+
+
+int main( int argc, char **argv ) {
+    cout << "Hello World from KVS_Server_Demo !" << endl;
+    printf("%s\n", "as");
+    return 0;
+}
